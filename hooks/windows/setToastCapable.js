69f6667cc241eb865f66dfdd871737711d84bfa4
Paolo Tesser <p.tesser921@gmail.com>
2020-04-17 15:34:14 +0200

    fix: update hook manifest url for fork repo

--- hooks/windows/setToastCapable.js
+++ hooks/windows/setToastCapable.js
@@ -1,4 +1,4 @@
-module.exports = function(context) {
+module.exports = function (context) {
     console.log('Updating appxmanifests with ToastCapable=true ...');
     var path = require('path');
     var fs = require('fs');
@@ -7,10 +7,10 @@ module.exports = function(context) {
     if (!fs.existsSync(platformProjPath)) {
         platformProjPath = context.opts.projectRoot;
     }
-    
+
     var AppxManifest = require(path.join(platformProjPath, 'cordova/lib/AppxManifest'));
 
-    ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function(manifestPath) {
+    ['package.phone.appxmanifest', 'package.windows.appxmanifest'].forEach(function (manifestPath) {
         var manifest = AppxManifest.get(path.join(platformProjPath, manifestPath));
         manifest.getVisualElements().setToastCapable(true);
         manifest.write();
